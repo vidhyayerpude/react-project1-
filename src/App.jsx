f77509678fe4792a0b7ab995037f6de1015da87d
vidhya yerpude <vidhyayerpude2@gmail.com>
2024-11-16 17:44:03 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+import HeroSection from "./components/Herosection";
+import Navigation from "./components/Navigation";
+
+
+
+const App = () => {
+  return (
+    <div>
+<Navigation/>
+<HeroSection/>
+
+     </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="ama" />
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+      <button>login</button>
+    </nav>
+  );
+};
+export default Navigation;
